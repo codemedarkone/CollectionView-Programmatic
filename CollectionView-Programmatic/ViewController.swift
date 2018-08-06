ef63605d9496830a7f01787c2ce37d64156dbd20
Chris <codemedarkone@gmail.com>
2018-08-05 20:16:13 -0600

    Initial Commit

---
+++ CollectionView-Programmatic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView-Programmatic
+//
+//  Created by chris  on 8/5/18.
+//  Copyright © 2018 kuronuma studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
