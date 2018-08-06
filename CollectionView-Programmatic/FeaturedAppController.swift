19d662bedcb4f56b91ddd10952b1c95633b2f24c
Chris <codemedarkone@gmail.com>
2018-08-05 20:25:10 -0600

    Created a collection View to hold my cells programmatically

---
+++ CollectionView-Programmatic/FeaturedAppController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CollectionView-Programmatic
+//
+//  Created by chris  on 8/5/18.
+//  Copyright © 2018 kuronuma studios. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        collectionView?.backgroundColor = UIColor.blue
+    }
+
+}
+
