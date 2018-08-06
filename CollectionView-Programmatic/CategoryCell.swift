ec5ca80db156b5d7fa2f056faaa8cb7fcb1aa0cb
Chris <codemedarkone@gmail.com>
2018-08-05 21:11:03 -0600

    made the horizontal cells (appCell) on top of the category cells

---
+++ CollectionView-Programmatic/CategoryCell.swift
@@ -0,0 +1,88 @@
+//
+//  CategoryCell.swift
+//  CollectionView-Programmatic
+//
+//  Created by chris  on 8/5/18.
+//  Copyright © 2018 kuronuma studios. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    
+    let cellId = "cellId"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    let appsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
+    func setupViews() {
+        backgroundColor = UIColor.clear
+     
+        addSubview(appsCollectionView)
+        appsCollectionView.dataSource = self
+        appsCollectionView.delegate = self
+        
+        appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: cellId)
+        
+        appsCollectionView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        appsCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8 ).isActive = true
+        appsCollectionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        appsCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8).isActive = true
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 100, height: frame.height)
+    }
+    
+}
+
+
+class AppCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.black
+        
+    }
+}
--- CollectionView-Programmatic/FeaturedAppController.swift
+++ CollectionView-Programmatic/FeaturedAppController.swift
@@ -40,21 +40,4 @@ class FeaturedAppController: UICollectionViewController, UICollectionViewDelegat
 }
 
 
-class CategoryCell: UICollectionViewCell {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setupViews()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupViews() {
-        backgroundColor = UIColor.blue
-        
-    }
-    
-}
+
